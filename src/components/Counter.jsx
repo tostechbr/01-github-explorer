64695de213f3cec8fa16b5e051c35cf681f324cd
Tiago Oliveira <tiagolivesan@outlook.com>
2021-07-16 22:04:17 -0300

    Fast Refresh no Webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -10,7 +10,12 @@ export function Counter() {
   return (
     <div>
       <h2>{counter}</h2>
-      <button type="button" onClick={incremente}>Increment</button>
+      <button
+        type="button"
+        onClick={increment}
+      >
+        Increment
+      </button>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -17,12 +18,14 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
